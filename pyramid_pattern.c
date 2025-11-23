9bb621d8ddee58a5aa0eab6eea774ccfb93ba36d
Tanish_demo <tanishjain6276@gmail.com>
2025-11-24 00:02:57 +0530

    first commit

---
+++ pyramid_pattern.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    for(int i=1; i<=n; i++){
+        for(int j=n; j>=1; j--){
+            if(j<=i || j==i || i==n){
+                printf("%-2d " , j);
+            }
+            else{
+            printf("   ");
+        }}
+        for(int j=2; j<=n; j++){
+            if(j<=i || i==n || j==i){
+                printf("%-2d " , j);
+            }
+            else{
+            printf("   ");
+        }
+        }
+    printf("\n");
+    }
+}
